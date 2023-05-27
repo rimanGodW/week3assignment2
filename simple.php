38a5c08738c791627afe113f6a444fadecd313f9
rimanGodW <134766920+rimanGodW@users.noreply.github.com>
2023-05-27 08:22:08 +0100

    Create simple.php

---
+++ simple.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Riman Godwin John";
+$regnumber = "2021/HND/33786/CS";
+$soo = "Cross River State";
+$topic = "Design an implementation of a web based logistics mgt system... A case study of nigerian breweries plc.. 9th mile corner. Enugu";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
